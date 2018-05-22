ff568441bf0772b9124e880f1e6b4570312cf23f
DanielCs88 <daniel.cs20@gmail.com>
2018-05-22 13:30:21 +0200

    Contains function

--- src/Player.ts
+++ src/Player.ts
@@ -1,33 +1,42 @@
 import {Gamestate} from "./gamestate.model";
+import {Card} from "./card.model";
 
 export class Player {
 
-  public betRequest(gameState: Gamestate, betCallback: (bet: number) => void): void {
-    console.log(gameState);
-      const activePlayers = gameState.players.filter(player => player.stack > 0);
-      console.log(activePlayers);
-      
-      let hero = null;
-      for (let player of activePlayers) {
+    public betRequest(gameState: Gamestate, betCallback: (bet: number) => void): void {
+        console.log(gameState);
+        const activePlayers = gameState.players.filter(player => player.stack > 0);
+        console.log(activePlayers);
+
+        let hero = null;
+        for (let player of activePlayers) {
           if (player.hasOwnProperty('hole_cards')) {
               hero = player;
           }
-      }
+        }
 
-      if (activePlayers.length > 3) {
+        if (activePlayers.length > 3) {
           betCallback(0);
           return;
-      }
-      if (hero.hole_cards.filter(card => card.rank == 'A').length > 0 || hero.hole_cards[0].rank == hero.hole_cards[1].rank) {
+        }
+        if (hero.hole_cards.filter(card => card.rank == 'A').length > 0 || hero.hole_cards[0].rank == hero.hole_cards[1].rank) {
           betCallback(hero.stack);
-      }else{
+        }else{
           betCallback(0);
-      }
+        }
+    }
 
-  }
+    private contains(card: string, range: string[]): boolean {
+        for (let item of range) {
+            if (item == card) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-  public showdown(gameState: Gamestate): void {
-  }
+    public showdown(gameState: Gamestate): void {
+    }
   
   
   
