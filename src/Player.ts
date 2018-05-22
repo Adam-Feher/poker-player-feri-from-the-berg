e2f9a1b058f1f54b3292e66d196d2d300bfcdf73
Adam Feher <fhradm@gmail.com>
2018-05-22 11:36:29 +0200

    pool < 3 bet with Ax

--- src/Player.ts
+++ src/Player.ts
@@ -4,11 +4,24 @@ export class Player {
 
   public betRequest(gameState: Gamestate, betCallback: (bet: number) => void): void {
     console.log(gameState);
-    betCallback(gameState.pot);
+      const activePlayers = gameState.players.filter(player => player.status == 'active');
+      const hero = gameState.players.find(player => player.hasOwnProperty('hole_cards'));
+
+      if (activePlayers.length > 2) {
+          betCallback(0);
+          return;
+      }
+      if (hero.hole_cards.filter(card => card.rank == 'A').length > 0) {
+          betCallback(hero.stack);
+      }
+
   }
 
   public showdown(gameState: Gamestate): void {
   }
+  
+  
+  
 }
 
 export default Player;
